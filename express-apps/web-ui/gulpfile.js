7f84e0acbcba38315f80a41f732f9b2bf7698f40
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2015-06-15 19:13:08 +0200

    feat(gulp): implement hierarchical gulpfile structure

---
+++ express-apps/web-ui/gulpfile.js
@@ -0,0 +1,58 @@
+var runSequence = require('run-sequence');
+
+module.exports = init;
+
+function init(gulp, plugins, prefix){
+
+  gulp = gulp || require('gulp');
+  plugins = plugins || require('gulp-load-plugins')();
+  prefix = prefix || '';
+  runSequence.use(gulp);
+
+  gulp.task(prefix + 'less', function(){
+
+    // Ignore less files starting with _
+    return gulp.src([
+      __dirname + '/less/**/*.less',
+      '!' + __dirname + '/less/**/_*.less'
+    ])
+      .pipe(plugins.sourcemaps.init())
+      .pipe(plugins.less())
+      .pipe(plugins.autoprefixer({
+        browsers: ['last 2 versions']
+      }))
+      .pipe(plugins.concat('app.css'))
+      .pipe(gulp.dest(__dirname + '/public/css'))
+      .pipe(plugins.minifyCss())
+      .pipe(plugins.rename('app.min.css'))
+      .pipe(plugins.sourcemaps.write('./'))
+      .pipe(gulp.dest(__dirname + '/public/css'));
+
+  });
+
+  gulp.task(prefix + 'watch:less', function(){
+
+    plugins.watch([
+      __dirname + '/less/**/*.less'
+    ], function(){
+      console.log('CHANGE DETECTED IN LESS');
+      runSequence(prefix + 'less');
+    });
+
+  });
+
+  gulp.task(prefix + 'default', function(){
+
+    runSequence(prefix + 'less', prefix + 'watch:less');
+
+  });
+
+}
+
+// @todo: is there a better way to check if we have been required or run directly using gulp?
+if (module.parent && module.parent.filename && module.parent.filename.match(/gulp\.js$/)) {
+  console.log('RUN DIRECTLY USING GULP');
+  init();
+}else{
+  console.log('REQUIRED');
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,11 @@
 var gulp = require('gulp');
-var less = require('gulp-less');
-var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
+var plugins = require('gulp-load-plugins')();
 var runSequence = require('run-sequence');
-var watch = require('gulp-watch');
-var concat = require('gulp-concat');
-var minifyCss = require('gulp-minify-css');
-var rename    = require('gulp-rename');
-var path = require('path');
 
-gulp.task('less', function () {
+require('./express-apps/web-ui/gulpfile')(gulp, plugins, 'web-ui:');
 
-  // Ignore less files starting with _
-  return gulp.src([
-      './express-app/less/**/*.less',
-      '!./express-app/less/**/_*.less'
-    ])
-    .pipe(sourcemaps.init())
-    .pipe(less())
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions']
-    }))
-    .pipe(concat('app.css'))
-    .pipe(gulp.dest('./express-app/public/css'))
-    .pipe(minifyCss())
-    .pipe(rename('app.min.css'))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./express-app/public/css'));
-});
+gulp.task('default', function () {
 
-gulp.task('watch-less', function(){
-  watch([
-    './express-app/less/**/*.less'
-  ], function(){
-    runSequence('less');
-  });
+  runSequence('web-ui:default');
+
 });
