4ba9702a3e6c12432c44a7cd429ea20558756838
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2015-06-16 17:34:12 +0200

    feat(sign-in): add component

---
+++ express-apps/web-ui/components/sign-in/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+module.exports = function(options){
+  return router;
+};
+
+router.get('/', function(req, res, next){
+  res.render(__dirname + '/views/index.jade');
+});
+
+router.post('/', function(req, res, next){
+
+  // Check if credentials are valid
+
+  res.render(__dirname + '/views/index.jade', {
+    invalidCredentials: true
+  });
+
+});
