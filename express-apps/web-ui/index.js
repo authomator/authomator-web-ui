7e25acbbc0fc93fa729ab3c6fa42c4e265615eac
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2015-06-16 17:35:30 +0200

    feat(express): add middleware

--- express-apps/web-ui/index.js
+++ express-apps/web-ui/index.js
@@ -3,6 +3,10 @@ var expressLayout = require('express-layout');
 var app = express();
 var logger = require('../../lib/logger');
 var config = require('config');
+var i18n = require('i18n');
+var lusca = require('lusca');
+var session = require('express-session');
+var bodyParser = require('body-parser');
 
 var defaultAppConfig = {
   locals: {
@@ -13,24 +17,12 @@ var defaultAppConfig = {
   }
 };
 
-module.exports = function(options) {
+module.exports = function (options) {
 
   config.util.extendDeep(defaultAppConfig, options)
   config.util.setModuleDefaults('app', defaultAppConfig);
 
   var appConfig = config.get('app');
-
-  /**************************************************************************
-   * Gather components
-   *
-   * Component specific options can be passed:
-   * require('./components/signup')(options)
-   *************************************************************************/
-
-  var components = {
-    signup: require('./components/signup')(),
-    errorHandlers: require('./components/error-handlers')()
-  };
 
   /**************************************************************************
    * Apply application settings
@@ -54,6 +46,45 @@ module.exports = function(options) {
   config.util.extendDeep(app.locals, appConfig.get('locals'));
 
   /**************************************************************************
+   * Configure body parser
+   *************************************************************************/
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: true}));
+
+  /**************************************************************************
+   * Configure i18n
+   *************************************************************************/
+
+  i18n.configure({
+    locales: ['en', 'nl'],
+    directory: __dirname + '/locales',
+    defaultLocale: 'en'
+  });
+
+  app.use(i18n.init);
+  app.use(function (req, res, next) {
+    console.log('Locale: ' + req.getLocale());
+    next();
+  });
+
+  /**************************************************************************
+   * Configure lusca
+   *************************************************************************/
+
+  app.use(session({
+    secret: 'Taths8yO5Heej3fiAj4pab8fIm5wetph',
+    resave: true,
+    saveUninitialized: true
+  }));
+
+  app.use(lusca({
+    csrf: true,
+    xframe: 'SAMEORIGIN',
+    xssProtection: true
+  }));
+
+  /**************************************************************************
    * Configure logger
    *************************************************************************/
 
@@ -63,15 +94,21 @@ module.exports = function(options) {
    * Configure layout
    *************************************************************************/
 
-  // Call layout middleware so all render calls from components
-  // are injected into the global layout
+    // Call layout middleware so all render calls from components
+    // are injected into the global layout
   app.use(expressLayout());
 
   /**************************************************************************
    * Load components
+   *
+   * Component specific options can be passed:
+   * require('./components/signup')(options)
    *************************************************************************/
 
-  app.use('/signup', components.signup);
+  app.use('/create-account', require('./components/create-account')());
+  app.use('/forgot-password', require('./components/forgot-password')());
+  app.use('/reset-password', require('./components/reset-password')());
+  app.use('/sign-in', require('./components/sign-in')());
 
   /**************************************************************************
    * Serve static assets
@@ -83,7 +120,7 @@ module.exports = function(options) {
    * Handle errors
    *************************************************************************/
 
-  app.use(components.errorHandlers);
+  app.use(require('./components/error-handlers')());
 
   return app;
 
