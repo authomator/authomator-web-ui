5733a95d6739ceb3ab0e284cfd019d1cc581cf2e
Stefan Lapers <stefan@lapers.be>
2015-06-19 15:30:30 +0200

    feat(reset-password): implement reset password

--- express-apps/web-ui/components/reset-password/index.js
+++ express-apps/web-ui/components/reset-password/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var Authomator = require('authomator-node-client');
+var errors = require('../../errors');
+var _ = require('lodash');
 
 module.exports = function(options){
   return router;
@@ -7,24 +10,23 @@ module.exports = function(options){
 
 router.get('/', function(req, res, next){
 
-  // If token still ok
   res.render(__dirname + '/views/index.jade');
-
-  // If token expired
-  res.render(__dirname + '/views/index.jade', {
-    invalidLink: true
-  });
-
 });
 
 router.post('/', function(req, res, next){
 
-  // If reset succeeded
-  res.render(__dirname + '/views/success.jade');
+  new Authomator().resetPassword(req.body.password, req.query.reset, function(err, tokens) {
 
-  // If token expired
-  //res.render(__dirname + '/views/index.jade', {
-  //  invalidLink: true
-  //});
+    if (err) {
 
+      if (!_.contains(['InvalidTokenError', 'NoSuchUserError', 'BadParamsError'], err.name)) return next(err);
+
+      return res.render(__dirname + '/views/index.jade', {
+        invalidToken: (err.name == 'InvalidTokenError' || err.name == 'NoSuchUserError'),
+        BadParamsError: (err.name == 'BadParamsError')
+      });
+    }
+
+    res.render(__dirname + '/views/success.jade');
+  });
 });
